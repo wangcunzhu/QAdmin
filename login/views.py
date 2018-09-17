c657ac9111e75dcc47a93566b6ec1439882fd054
wangcunzhu <564286058@qq.com>
2018-09-17 12:15:34 +0800

    test

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 
@@ -12,17 +12,21 @@ from django.contrib.auth import authenticate, login
 #     return render(request, "login/login.html")
 
 def do_login(request):
+    # print("11111111111")
+    # print(request.path)
+    # if request.method == 'GET':
+    #     print("22222222")
+    #     return render(request, 'login/login.html')
     print("11111111111")
-    if request.method == 'GET':
-        print("22222222")
-        return render(request, 'login/login.html')
-
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    print(username)
+    print(password)
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return redirect(reverse('login'))
+        # return redirect(reverse('login'))
+        return render(request, 'casetest/plain_page.html')
     else:
         return render(request, 'login/login.html', {
             'username': username,
